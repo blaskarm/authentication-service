10e84552ebf9e6218440c03374669b7d98a7ee52
blaskarm <emillindhult@gmail.com>
2025-11-26 13:26:15 +0100

    testing merge

--- AuthenticationService.API/Program.cs
+++ AuthenticationService.API/Program.cs
@@ -14,6 +14,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Testing merge
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
